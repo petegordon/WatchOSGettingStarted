49bac992ffc334ea2ba65c37ac07fde159ee92ce
Pete Gordon <pete@petegordon.com>
2017-07-29 16:34:11 -0400

    Initial Commit

---
+++ WatchOSGettingStarted/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchOSGettingStarted
+//
+//  Created by Pete Gordon on 7/29/17.
+//  Copyright © 2017 UsersFirst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
