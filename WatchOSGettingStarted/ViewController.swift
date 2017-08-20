908a7a1577c72f5de144393c1456eb27a7981399
Pete Gordon <pete@petegordon.com>
2017-08-20 17:10:56 -0400

    Test App for WatchOS Dictation, Audio Recording... and iPhone SiriKit

--- WatchOSGettingStarted/ViewController.swift
+++ WatchOSGettingStarted/ViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSpeechRecognizerDelegate  {
 
+    @IBOutlet weak var btnStartStop: UIButton!
+    @IBOutlet weak var textOutput: UILabel!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        recordAndRecognizeSpeech()
+    }
+    
+    func recordAndRecognizeSpeech(){
+        guard let node = audioEngine.inputNode else { return }
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat){ buffer, _ in self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do{
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            //A recognizer is not supported for the current locale
+            return
+        }
+        if(!myRecognizer.isAvailable){
+            //A recognizer is not available right now.
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: {result, error in
+            if let result = result {
+                
+                let bestString = result.bestTranscription.formattedString
+                self.textOutput.text = bestString
+                
+            }else if let error = error {
+                print(error)
+            }
+        })
+    }
 
 }
 
